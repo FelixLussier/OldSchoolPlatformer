626ed4a899e4e94f6c476d2e37513d1293c06f9c
Félix Lussier <williamblackthorn@gmail.com>
2019-02-19 11:15:18 -0500

    Travail sur FSM

---
+++ Old School Platformer/Assets/Scripts/FSMState.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FSMState 
+{
+    private readonly string name;
+    private readonly FSM owner;
+    private List<FSMAction> actions;
+    private readonly Dictionary<string, FSMState> transitionMap;
+
+    public FSMState(string name, FSM owner)
+    {
+        this.name = name;
+        this.owner = owner;
+        this.transitionMap = new Dictionary<string, FSMState>();
+        this.actions = new List<FSMAction>();
+    }
+
+
+    public void AddTransition(string id, FSMState destinationState)
+    {
+        if(transitionMap.ContainsKey(id))
+        {
+            Debug.LogError(string.Format("State {0} already contains transition for {1}", this.name, id));
+            return;
+        }
+
+        transitionMap[id] = destinationState;    
+    }
+
+
+    /*public FSMState GetTransition(string eventId)
+    {
+       
+    }*/
+
+    public void AddAction(FSMAction action)
+    {
+
+    }
+
+    public IEnumerable<FSMAction> GetActions()
+    {
+        return actions;
+    }
+
+    public void SendEvent(string eventid)
+    {
+
+    }
+}
