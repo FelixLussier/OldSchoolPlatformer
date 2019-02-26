39d777a2379f1661b0c328a6809c34ab371f79c4
Alexis Paquette <paquetteale@hotmail.ca>
2019-02-26 10:55:44 -0500

    Ajout bouton Mute

---
+++ Old School Platformer/Assets/BoutonMute.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class BoutonMute : MonoBehaviour
+{
+    Slider sliderVolume;
+
+    private void Start()
+    {
+        sliderVolume = GameObject.Find("Slider").GetComponent<Slider>();
+
+        sliderVolume.value = -35;
+        AudioListener.volume = -35;
+
+    }
+
+}
--- Old School Platformer/Assets/SettingsMenu.cs
+++ Old School Platformer/Assets/SettingsMenu.cs
@@ -2,14 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
 
     public AudioMixer audioMixer;
+    public Slider sliderVolume;
+    public Toggle boutonToggle;
 
     public void ChangeVolume(float volume)
     {
         audioMixer.SetFloat("volume", volume);
     }
+
+    public void
+
 }
