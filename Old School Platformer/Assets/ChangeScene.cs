c9ff795b510ceb6670e20486d4bb78ebeca2cf6f
Alexis Paquette <paquetteale@hotmail.ca>
2019-02-26 09:01:04 -0500

    Ajout volume

--- Old School Platformer/Assets/ChangeScene.cs
+++ Old School Platformer/Assets/ChangeScene.cs
@@ -8,7 +8,7 @@ public class ChangeScene : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //Vérifie sur quelle boîte le joueur entre en collision
+        //Vérifie avec quelle boîte le joueur entre en collision
         if(gameObject.name == "BoxBouton2")
         {
             SceneManager.LoadScene("SampleScene");
