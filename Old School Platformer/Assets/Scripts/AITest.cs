e9289abe8f708fddeebe79ed3e4e50ef1204b28e
Félix Lussier <williamblackthorn@gmail.com>
2019-02-26 10:53:33 -0500

    Première phase de test réussi sur l'AI

--- Old School Platformer/Assets/Scripts/AITest.cs
+++ Old School Platformer/Assets/Scripts/AITest.cs
@@ -11,7 +11,9 @@ public class AITest : MonoBehaviour
     private TestAction1 testAction1;
     private TestAction1 testAction2;
     private TestAction1 testAction3;
-
+    private List<string> finishEvent1;
+    private List<string> finishEvent2;
+    private List<string> finishEvent3;
 
 
     // Start is called before the first frame update
@@ -25,6 +27,22 @@ public class AITest : MonoBehaviour
         testAction2 = new TestAction1(testState2);
         testAction3 = new TestAction1(testState3);
 
+        finishEvent1 = new List<string>
+        {
+            "To 2",
+            "To 3"
+        };
+        finishEvent2 = new List<string>
+        {
+            "To 1",
+            
+        };
+        finishEvent3 = new List<string>
+        {
+            "To 1",
+            
+        };
+
         testState1.AddAction(testAction1);
         testState2.AddAction(testAction2);
         testState3.AddAction(testAction3);
@@ -33,17 +51,19 @@ public class AITest : MonoBehaviour
         testState1.AddTransition("To 2", testState2);
         testState1.AddTransition("To 3", testState3);
 
-        testState2.AddTransition("to 1", testState1);
-        testState3.AddTransition("to 1", testState1);
+        testState2.AddTransition("To 1", testState1);
+        testState3.AddTransition("To 1", testState1);
 
-        testAction1.init("test 1",2,0f,)
-
+        testAction1.init("test 1", 1f, finishEvent1);
+        testAction2.init("test 2", 1f, finishEvent2);
+        testAction3.init("test 3", 1f, finishEvent3);
+        fsm.Start("testState1");
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        fsm.Update();
     }
 }
--- Old School Platformer/Assets/Scripts/TestAction1.cs
+++ Old School Platformer/Assets/Scripts/TestAction1.cs
@@ -9,6 +9,10 @@ public class TestAction1 : FSMAction
     private float duration;
     private float cachedDuration;
     private List<string> finishEvent;
+    private int length= 0;
+    private System.Random rand = new System.Random();
+    private int random;
+
 
 
 
@@ -18,12 +22,15 @@ public class TestAction1 : FSMAction
     }
  
     
-    public void init(string textToShow, float duration,List<string> finishEvent)
+    public void init(string textToShow, float duration,List<string> finishEvent1)
     {
         this.textToShow = textToShow;
         this.duration = duration;
+        this.finishEvent = finishEvent1;
         this.cachedDuration = duration;
-        this.finishEvent = finishEvent;
+        
+
+
     }
 
     public override void OnEnter()
@@ -41,6 +48,7 @@ public class TestAction1 : FSMAction
         duration -= Time.deltaTime;
         if(duration <= 0)
         {
+
             Finish();
             return;
         }
@@ -56,8 +64,23 @@ public class TestAction1 : FSMAction
 
     public void Finish()
     {
-       
+        
+        
+        
+        this.length = this.finishEvent.Count;
+        
+
+        random = this.rand.Next(0, length);
+        //random -= 1;
+
+        if(!string.IsNullOrEmpty(this.finishEvent[random]))
+        GetOwner().SendEvent(this.finishEvent[random]);
+
+
+
         duration = cachedDuration;
+
+
     }
 
 
