55b8df589e458893552fe49376356a1ac98c9c20
felix <felix>
2019-04-09 11:05:46 -0400

    Début de la programmation tu vrai boss

---
+++ Old School Platformer/Assets/Scripts/Boss.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    private FSM fsm;
+    private FSMState standState;
+    private FSMState moveTargetState;
+    private MoveTargetAction moveTargetAction;
+    private StandAction standAction;
+    private List<string> finishEventStand;
+    private List<string> finishEventMoveTarget;
+    private Vector3 vecPos;
+    private Vector3 vecEnnemis;
+    
+
+    void Start()
+    {
+        fsm = new FSM("Boss Ai");
+
+        standState = fsm.AddState("StandState");
+        moveTargetState = fsm.AddState("MoveTargetState");
+
+
+        standAction = new StandAction(standState);
+        moveTargetAction = new MoveTargetAction(moveTargetState);
+
+
+        finishEventStand = new List<string>
+        {
+            "to move target"
+           
+        };
+
+        finishEventMoveTarget = new List<string>
+        {
+            "to move target",
+            "to stand"
+
+        };
+
+        standState.AddAction(standAction);
+
+        moveTargetState.AddAction(moveTargetAction);
+
+        standState.AddTransition("to move target", moveTargetState);
+
+        moveTargetState.AddTransition("to move target", moveTargetState);
+        moveTargetState.AddTransition("to stand", standState);
+
+        vecPos = GameObject.Find("Boss").transform.position;
+        vecEnnemis = GameObject.Find("AI").transform.position;
+
+        moveTargetAction.init(this.transform, vecPos, vecEnnemis, 2.0f, finishEventMoveTarget);
+        standAction.init(1.0f, finishEventStand);
+
+        fsm.Start("StandState");
+    }
+
+    
+    void Update()
+    {
+        fsm.Update();
+    }
+}
---
+++ Old School Platformer/Assets/Scripts/MoveTargetAction.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTargetAction : FSMAction
+{
+    private Transform transform;
+    private float duration;
+    private float cachedDuration;
+    private float journeyLength;
+    private float polledTime;
+    private List<string> finishEvent;
+    private int length = 0;
+    private System.Random rand = new System.Random();
+    private int random;
+    private Vector3 positionFrom;
+    private Vector3 positionTo;
+    private Vector3 positionToW;
+
+
+
+
+    public MoveTargetAction(FSMState owner) : base(owner)
+    {
+
+    }
+
+
+    public void init(Transform transform, Vector3 from, Vector3 to, float duration, List<string> finishEventList)
+    {
+
+        this.duration = duration;
+        this.finishEvent = finishEventList;
+        this.cachedDuration = duration;
+        this.transform = transform;
+        this.positionFrom = from;
+        this.positionTo = to;
+        this.positionToW = new Vector3(0.4f,0,0);
+        this.polledTime = 0;
+        this.journeyLength = Vector3.Distance(this.positionFrom, this.positionTo);
+
+    }
+
+    public override void OnEnter()
+    {
+        if (duration <= 0)
+        {
+            Finish();
+            return;
+        }
+        SetPosition(this.positionFrom);
+    }
+
+    public override void OnUpdate()
+    {
+        polledTime += Time.deltaTime;
+        duration -= Time.deltaTime;
+        random = this.rand.Next(0, 13);
+
+        if (duration <= 0)
+        {
+
+            Finish();
+            return;
+        }
+
+        /*if (random == 0)
+        {
+            Finish();
+
+            return;
+        }
+        */
+
+
+        this.positionFrom = GameObject.Find("Boss").transform.position;
+        if(GameObject.Find("Boss").transform.position.x< GameObject.Find("AI").transform.position.x)
+        {
+            this.positionTo = this.positionFrom + positionToW;
+        }
+        else
+        {
+            this.positionTo = this.positionFrom - positionToW;
+        }
+
+        //transform.position = Vector3.MoveTowards(this.positionFrom, this.positionTo, 0.005f*Time.deltaTime);
+        SetPosition(Vector3.Lerp(this.positionFrom, this.positionTo, Mathf.Clamp(polledTime / cachedDuration, 0, 1)));
+    }
+
+
+    public override void OnExit()
+    {
+
+    }
+
+
+    public void Finish()
+    {
+
+
+
+        this.length = this.finishEvent.Count;
+
+
+        random = this.rand.Next(0, length);
+
+
+        if (!string.IsNullOrEmpty(this.finishEvent[random]))
+            GetOwner().SendEvent(this.finishEvent[random]);
+
+        //SetPosition(this.positionFrom + this.positionToW);
+        SetPosition(Vector3.Lerp(this.positionFrom, this.positionTo, Mathf.Clamp(polledTime / cachedDuration, 0, 2)));
+        this.polledTime = 0;
+        this.journeyLength = Vector3.Distance(this.positionFrom, this.positionTo);
+
+        duration = cachedDuration;
+
+
+    }
+
+
+    private void SetPosition(Vector3 position)
+    {
+        this.transform.position = position;
+    }
+}
