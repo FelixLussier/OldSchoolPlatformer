05e0e35df5e4c2dbd3113433fe685e1561267975
Lefebvre Tomy <)>
2019-05-01 22:45:44 -0400

    Merge des scripts de Collisions avec master.

---
+++ Old School Platformer/Assets/Scripts/Alive.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ 
+    Tous les objects qui peuvent mourir doivent heriter de Alive.
+
+ */
+
+public class Alive : Collision
+{
+    public int life;
+
+    void Start()
+    {
+        life = 1;
+
+        rigidbodyComp = GetComponent<Rigidbody2D>();
+        rigidbodyComp.isKinematic = true;
+
+        GetComponent<BoxCollider2D>().isTrigger = true;
+    }
+
+    
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isAlive())
+            die();
+    }
+
+    virtual public void takeDamage(int damage) // L'objet perd de la vie.
+    {
+        life -= damage;
+    }
+
+    public void die() //! Destroy the object
+    {
+        Destroy(this.gameObject);
+    }
+    
+    public bool isAlive()
+    {
+        return life > 0;
+    }
+
+    public int getLife()
+    {
+        return life;
+    }
+}
---
+++ Old School Platformer/Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Player : Alive
+{
+    Projectile proj;
+
+    void Start()
+    {
+        rigidbodyComp = GetComponent<Rigidbody2D>();
+            rigidbodyComp.isKinematic = true;
+
+        GetComponent<BoxCollider2D>().isTrigger = true;
+
+        speed.x = 0.00F;
+        speed.y = 0.01F;
+
+        life = 1;
+
+        Projectile proj = Instantiate(Resources.Load("proj")) as Projectile;
+    }
+    void Update()
+    {
+    }
+    
+    void FixedUpdate()
+    {
+        actualiseMouvement();
+
+        checkCollisions();
+
+        if (!isAlive())
+            die();
+    }
+}
